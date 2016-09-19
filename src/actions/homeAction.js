28a2e757f7f5151eb357e752ed0f1ed5d26eefc4
黄 家金 <hjj245148>
2016-09-19 19:33:52 +0800

    all

---
+++ src/actions/homeAction.js
@@ -0,0 +1,104 @@
+import * as types from '../constants/ActionTypes';
+import * as APIs from '../constants/ServerAPIs';
+
+
+function shouldFetchHomeList(state) {
+    const data = state.homeList;
+    if (data && data.isFetchingHomeList) {
+        return false
+    }
+    return true;
+}
+
+function shouldFetchMoreHomeList(state) {
+    const data = state.homeList;
+    if (data && data.isFetchingMoreList) {
+        return false
+    }
+    return true;
+}
+//首次获取列表
+export function fetchAllHomeList() {
+
+    return async (dispatch, getState) => {
+        //verify
+        if (!shouldFetchHomeList(getState())) {
+            return Promise.resolve();
+        }
+
+        //dispatch start fetch action
+        dispatch({type: types.START_FETCH_ALL_HOME_LIST});
+
+        //fetching
+        const response = await fetch(APIs.homeList);
+        //response
+        const data = await response.json();
+
+        //dispatch end fetch action
+        return dispatch({
+            type: types.FETCH_ALL_HOME_LIST,
+            data
+        });
+    }
+}
+
+
+
+//刷新获取
+export function fetchRefreshHomeList() {
+
+    return async (dispatch, getState) => {
+        //verify
+        if (!shouldFetchHomeList(getState())) {
+            return Promise.resolve();
+        }
+
+        //dispatch start fetch action
+        dispatch({type: types.START_FETCH_ALL_HOME_LIST});
+
+        //fetching
+        var olddata=getState().homeList.data;
+        const response = await fetch(APIs.homeList+"?type=refresh&created="+olddata[0].created);
+        //response
+        const data = await response.json();
+        //dispatch end fetch action
+        return dispatch({
+            type: types.RERESH_HOME_LIST,
+            data
+        });
+    }
+}
+
+//获取更多
+export function fetchMoreHomeList() {
+
+    return async (dispatch, getState) => {
+        //verify
+        if (!shouldFetchMoreHomeList(getState())) {
+            return Promise.resolve();
+        }
+
+        //dispatch start fetch action
+        dispatch({type: types.START_FETCH_MORE_HOME_LIST});
+
+        //fetching
+        var olddata=getState().homeList.data;
+        const response = await fetch(APIs.homeList+"?type=more&created="+olddata[olddata.length-1].created);
+        //response
+        const data = await response.json();
+        //dispatch end fetch action注意 加载太快刷新组件有bug
+        return dispatch({
+            type: types.MORE_HOME_LIST,
+            data
+        });
+
+    }
+}
+
+
+
+
+
+
+
+
---
+++ src/reducers/homeReducer.js
@@ -0,0 +1,48 @@
+import * as types from '../constants/ActionTypes';
+import Utils from '../utils';
+
+
+const initialState = {
+    isFetchingHomeList: false,
+    isFetchingMoreList:false,
+    data: []
+}
+
+
+export default function homeList(state = initialState, action) {
+    switch (action.type) {
+        case types.START_FETCH_ALL_HOME_LIST:
+            return Object.assign({}, state, {isFetchingHomeList: true,isFetchingMoreList:false});
+        case types.START_FETCH_MORE_HOME_LIST:
+            return Object.assign({}, state, {isFetchingMoreList: true,isFetchingHomeList:false});
+
+        case types.FETCH_ALL_HOME_LIST:
+            //console.log('FETCH_ALL_HOME_LIST')
+
+            var data=state.data.concat(action.data)
+            var newdata= Object.assign({},{data:data},{isFetchingMoreList: false,isFetchingHomeList:false});
+            //console.log(newdata,'newdata')
+            return newdata;
+        case types.RERESH_HOME_LIST:
+            if(action.data.length>0){
+                var data=action.data.concat(state.data)
+                var newdata= Object.assign({},{data:data},{isFetchingMoreList: false,isFetchingHomeList:false});
+                return newdata
+            }
+            var newdata= Object.assign({},{data:state.data},{isFetchingMoreList: false,isFetchingHomeList:false});
+            return newdata;
+
+        case types.MORE_HOME_LIST:
+            if(action.data.length>0){
+                var data=state.data.concat(action.data);
+                var newdata= Object.assign({},{data:data},{isFetchingMoreList: false,isFetchingHomeList:false});
+                return newdata
+            }
+            var newdata= Object.assign({},{data:state.data},{isFetchingMoreList: false,isFetchingHomeList:false});
+            return newdata;
+
+        default:
+            //console.log('default',state)
+            return state
+    }
+}
